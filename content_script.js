f5145fdbf89acb2f19531c1ac277d5e35266be9a
Wendelin Keimel <wendelin.keimel@aboutsource.net>
2022-01-24 16:33:45 +0100

    add button

--- content_script.js
+++ content_script.js
@@ -1,1 +1,3 @@
-//TODO: Code, der auf trello.com ausgeführt wird
+const sidebar = document.getElementsByClassName("window-sidebar")[0]
+sidebar.innerHTML = "<span class='button-link'><span class='icon-sm plugin-icon'>+</span><span>Toggl Task</span></span>" + sidebar.innerHTML
+sidebar.getElementsByClassName("mod-no-top-margin")[1].classList.remove("mod-no-top-margin")
