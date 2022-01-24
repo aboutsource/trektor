21ac52e8bf9cb907edde3ed974db37afcb6fd850
Wendelin Keimel <wendelin.keimel@aboutsource.net>
2022-01-24 16:43:48 +0100

    add button event handler

--- content_script.js
+++ content_script.js
@@ -1,3 +1,10 @@
 const sidebar = document.getElementsByClassName("window-sidebar")[0]
-sidebar.innerHTML = "<span class='button-link'><span class='icon-sm plugin-icon'>+</span><span>Toggl Task</span></span>" + sidebar.innerHTML
+sidebar.innerHTML = "<span class='button-link' id='togglbtn'><span class='icon-sm plugin-icon'>+</span><span>Toggl Task</span></span>" + sidebar.innerHTML
+
 sidebar.getElementsByClassName("mod-no-top-margin")[1].classList.remove("mod-no-top-margin")
+
+document.getElementById("togglbtn").addEventListener("click", onClick)
+
+function onClick(){
+    alert("work in progress ;)")
+}
