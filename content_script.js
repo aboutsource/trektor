424b073b04ffda5f3f32f8cfd9aa27f78c99db6a
Wendelin Keimel <wendelin.keimel@aboutsource.net>
2022-01-25 09:38:12 +0100

    add onload event handler

--- content_script.js
+++ content_script.js
@@ -1,10 +1,15 @@
 const sidebar = document.getElementsByClassName("window-sidebar")[0]
-sidebar.innerHTML = "<span class='button-link' id='togglbtn'><span class='icon-sm plugin-icon'>+</span><span>Toggl Task</span></span>" + sidebar.innerHTML
 
-sidebar.getElementsByClassName("mod-no-top-margin")[1].classList.remove("mod-no-top-margin")
-
-document.getElementById("togglbtn").addEventListener("click", onClick)
+function addButton(){
+    sidebar.innerHTML = "<span class='button-link' id='togglbtn'><span class='icon-sm plugin-icon'>+</span><span>Toggl Task</span></span>" + sidebar.innerHTML
+    sidebar.getElementsByClassName("mod-no-top-margin")[1].classList.remove("mod-no-top-margin")
+    document.getElementById("togglbtn").addEventListener("click", onClick)
+}
 
 function onClick(){
     alert("work in progress ;)")
 }
+
+addButton()
+
+document.getElementById("board").addEventListener("click", addButton)
