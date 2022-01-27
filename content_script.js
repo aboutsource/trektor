52d09ec2179796da5a5fbd31e814367673087c39
Wendelin Keimel <wendelin.keimel@aboutsource.net>
2022-01-27 14:10:47 +0100

    make tokens configurable

--- content_script.js
+++ content_script.js
@@ -29,77 +29,62 @@ const mappings = {
 };
 
 function onClick() {
-    let apiKey = "***"
-    //generate token at https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Server%20Token&key=afadffe77f745496f80ebb4bf460c615
-    let token = "af00d9597af9fb897f6b2fedb3b7152845991fe186caca08cf5dbb03bba40ffc"
-    let togglToken = "***"
+    let trelloApiKey = "afadffe77f745496f80ebb4bf460c615"
+    browser.storage.local.get().then(result => {
+        const trelloToken = result.trello
+        const togglToken = result.toggl
 
-    const idLong = window.location.pathname.split("/")[2];
-    var url = new URL(`https://api.trello.com/1/cards/${idLong}?key=${apiKey}&token=${token}`)
-    fetch(url.toString()).then(response => response.json()).then(response => {
-        const idShort = response["idShort"]
+        trelloToken = "af00d9597af9fb897f6b2fedb3b7152845991fe186caca08cf5dbb03bba40ffc"
+        togglToken = "1ccd218cb782e37aa2a50ed6b546272e"
 
-        console.log("Hallo")
-        var labelShort, labelLong = undefined
-        for (var i in response["labels"]) {
+        const idLong = window.location.pathname.split("/")[2];
+        var url = new URL(`https://api.trello.com/1/cards/${idLong}?key=${trelloApiKey}&token=${trelloToken}`)
+        fetch(url.toString()).then(response => response.json()).then(response => {
+            const idShort = response["idShort"]
+
+            var labelShort, labelLong = undefined
+            for (var i in response["labels"]) {
+                if (labelShort == undefined) {
+                    labelShort = mappings[response["labels"][i]["name"]]
+                    labelLong = (labelShort != undefined) ? response["labels"][i]["name"] : labelLong
+                } else if (mappings[response["labels"][i]["name"]] != undefined) {
+                    alert(`Diese Karte hat sowohl "${labelShort}" als auch "${mappings[response["labels"][i]["name"]]}".`)
+                }
+            }
+
             if (labelShort == undefined) {
-                labelShort = mappings[response["labels"][i]["name"]]
-                labelLong = (labelShort != undefined) ? response["labels"][i]["name"] : labelLong
-            } else if (mappings[response["labels"][i]["name"]] != undefined) {
-                alert(`Diese Karte hat sowohl "${labelShort}" als auch "${mappings[response["labels"][i]["name"]]}".`)
-            }
-        }
-
-        if (labelShort == undefined) {
-            alert("Diese Karte besitzt kein in den mappings vorhandenes Label.")
-            return false
-        }
-
-        if (!response["name"].endsWith(`#${labelShort}_${idShort}`)) {
-
-
-            url = new URL(`https://api.trello.com/1/cards/${idLong}`)
-
-            var data = {
-                "name": `${response["name"]} #${labelShort}_${idShort}`
-            };
-            fetch(url.toString(), {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `OAuth oauth_consumer_key="${apiKey}", oauth_token="${token}"`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            }).then(response => {
-                if (!response.ok) {
-                    alert(`Error ${response.status}:\n${response.statusText}`)
-                    return false
-                }
-
-            })
-
-
-        }
-
-        url = new URL("https://api.track.toggl.com/api/v8/workspaces")
-        const togglAuth = btoa(`${togglToken}:api_token`)
-        fetch(url.toString(), {
-            headers: {
-                'Authorization': `Basic ${togglAuth}`,
-                'Content-Type': 'application/json'
-            }
-        }).then(response => response.json()).then(response => {
-            for (var i in response) {
-                if (response[i]["name"] == "aboutsource") {
-                    var wid = response[i]["id"]
-                }
-            }
-            if (wid == undefined) {
-                alert("WorkspaceID undefined\nDies bedeutet, du hast entweder keinen Zugriff zum a:s toggl oder es ist kein API-Token angegeben.")
+                alert("Diese Karte besitzt kein in den mappings vorhandenes Label.")
                 return false
             }
 
-            url = new URL(`https://api.track.toggl.com/api/v8/workspaces/${wid}/projects`)
+            if (!response["name"].endsWith(`#${labelShort}_${idShort}`)) {
+
+
+                url = new URL(`https://api.trello.com/1/cards/${idLong}`)
+
+                var data = {
+                    "name": `${response["name"]} #${labelShort}_${idShort}`
+                };
+                fetch(url.toString(), {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `OAuth oauth_consumer_key="${trelloApiKey}", oauth_token="${trelloToken}"`,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                }).then(response => {
+                    if (!response.ok) {
+                        alert(`Error ${response.status}:\n${response.statusText}`)
+                        return false
+                    }
+
+                })
+
+
+            }
+
+            url = new URL("https://api.track.toggl.com/api/v8/workspaces")
+            const togglAuth = btoa(`${togglToken}:api_token`)
             fetch(url.toString(), {
                 headers: {
                     'Authorization': `Basic ${togglAuth}`,
@@ -107,36 +92,54 @@ function onClick() {
                 }
             }).then(response => response.json()).then(response => {
                 for (var i in response) {
-                    if (response[i]["name"].endsWith(`(${labelShort})`)) {
-                        var pid = response[i]["id"]
-
-                        url = new URL('https://api.track.toggl.com/api/v8/tasks')
-                        data = {
-                            "task": {
-                                "name": `${labelShort}_${idShort}`,
-                                "pid": pid
-                            }
-                        }
-                        fetch(url.toString(), {
-                            method: 'POST',
-                            headers: {
-                                'Authorization': `Basic ${togglAuth}`,
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify(data)
-                        }).then(response => {
-                            console.log(response)
-                        })
+                    if (response[i]["name"] == "aboutsource") {
+                        var wid = response[i]["id"]
                     }
                 }
-            })
-        })
+                if (wid == undefined) {
+                    alert("WorkspaceID undefined\nDies bedeutet, du hast entweder keinen Zugriff zum a:s toggl oder es ist kein API-Token angegeben.")
+                    return false
+                }
 
+                url = new URL(`https://api.track.toggl.com/api/v8/workspaces/${wid}/projects`)
+                fetch(url.toString(), {
+                    headers: {
+                        'Authorization': `Basic ${togglAuth}`,
+                        'Content-Type': 'application/json'
+                    }
+                }).then(response => response.json()).then(response => {
+                    for (var i in response) {
+                        if (response[i]["name"].endsWith(`(${labelShort})`)) {
+                            var pid = response[i]["id"]
+
+                            url = new URL('https://api.track.toggl.com/api/v8/tasks')
+                            data = {
+                                "task": {
+                                    "name": `${labelShort}_${idShort}`,
+                                    "pid": pid
+                                }
+                            }
+                            fetch(url.toString(), {
+                                method: 'POST',
+                                headers: {
+                                    'Authorization': `Basic ${togglAuth}`,
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(data)
+                            }).then(response => {
+                                console.log(response)
+                            })
+                        }
+                    }
+                })
+            })
+
+        })
+    })
+
+    window.addEventListener("pushstate", function () {
+        if (window.location.pathname.startsWith("/c/")) {
+            addButton()
+        }
     })
 }
-
-window.addEventListener("pushstate", function () {
-    if (window.location.pathname.startsWith("/c/")) {
-        addButton()
-    }
-})
--- options/script.js
+++ options/script.js
@@ -1,1 +1,21 @@
-//extension settings script
+const trelloTextField = document.querySelector("#trello_token")
+const togglTextField = document.querySelector("#toggl_token")
+
+trelloTextField.addEventListener("change", onChange)
+trelloTextField.addEventListener("paste", onChange)
+togglTextField.addEventListener("change", onChange)
+togglTextField.addEventListener("paste", onChange)
+
+function onChange() {
+    browser.storage.local.set({
+        "trello": trelloTextField.value,
+        "toggl": togglTextField.value
+    })
+    console.log(`set trello token to ${togglTextField.value}`)
+    console.log(`set toggl token to ${togglTextField.value}`)
+}
+
+browser.storage.local.get().then(result => {
+    trelloTextField.value = result["trello"]
+    togglTextField.value = result["toggl"]
+})
