db00c6fe41335fcb5f193bd21197601a50ae167a
Martin Kalcher <martin.kalcher@aboutsource.net>
2022-02-02 18:22:16 +0100

    some cleanup

--- trektor.js
+++ trektor.js
@@ -1,22 +1,22 @@
 class ChromeStorage {
-  constructor() {
-      this.storage = chrome.storage.local;
+  constructor(storage) {
+    this.storage = storage;
   }
   get(keys) {
-      return new Promise((resolve) => {
-          this.storage.get(keys, resolve);
-      })
+    return new Promise((resolve) => {
+      this.storage.get(keys, resolve);
+    })
   }
   set(values) {
-      return new Promise((resolve) => {
-          this.storage.set(values, resolve);
-      })
+    return new Promise((resolve) => {
+      this.storage.set(values, resolve);
+    })
   }
 }
 
 class ChromeRuntime {
-  constructor() {
-    this.runtime = chrome.runtime;
+  constructor(runtime) {
+    this.runtime = runtime;
   }
 
   sendMessage(msg) {
@@ -25,30 +25,106 @@ class ChromeRuntime {
     });
   }
 
-  onMessage(callback) {
-    this.runtime.onMessage.addListener((msg, sender, respond) => {
-      callback(msg).then((response) => { respond(response) });
-      return true;
-    });
+  get onMessage() {
+    const runtime = this.runtime;
+
+    return {
+      addListener(callback) {
+        runtime.onMessage.addListener((msg, sender, respond) => {
+          callback(msg)
+            .then((response) => { respond(response) })
+            .catch((error) => respond(error.message));
+          return true;
+        });
+      },
+    }
   }
 }
 
-window.trektor = {
-  storage: null,
-  runtime: null,
+class TrelloGateway {
+  static ENDPOINT = "https://api.trello.com/1";
+  static API_KEY = "afadffe77f745496f80ebb4bf460c615";
 
-  fetchJSON(url, options = {}) {
-    return this.runtime.sendMessage({
-      action: 'fetchJSON',
-      args: [url, options],
+  constructor(storage) {
+    this.storage = storage;
+  }
+
+  getCard(id) {
+    return this.request("get", `/cards/${id}`);
+  }
+
+  updateCard(id, data) {
+    return this.request("put", `/cards/${id}`, data);
+  }
+
+  async request(method, path, data = null) {
+    const url = this.constructor.ENDPOINT + path;
+    const { trello: token } = await this.storage.get(["trello"]);
+
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Authorization": `OAuth oauth_consumer_key="${this.constructor.API_KEY}", oauth_token="${token}"`,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: (data === null) ? null : JSON.stringify(data),
     });
-  },
-};
+
+    return response.json();
+  }
+}
+
+class TogglGateway {
+  static ENDPOINT = "https://api.track.toggl.com/api/v8";
+
+  constructor(storage) {
+    this.storage = storage;
+  }
+
+  getWorkspaces() {
+    return this.request("get", "/workspaces");
+  }
+
+  getProjects(workspaceId) {
+    return this.request("get", `/workspaces/${workspaceId}/projects`);
+  }
+
+  getTasks(projectId) {
+    return this.request("get", `/projects/${projectId}/tasks`);
+  }
+
+  createTask(projectId, name) {
+    return this.request("post", "/tasks", { task: { name, pid: projectId } });
+  }
+
+  async request(method, path, data = null) {
+    const url = this.constructor.ENDPOINT + path;
+    const { toggl: token } = await this.storage.get(["toggl"]);
+
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Authorization": `Basic ${btoa(`${token}:api_token`)}`,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: (data === null) ? null : JSON.stringify(data),
+    });
+
+    return response.json();
+  }
+}
+
+const trektor = {};
 
 if (window.chrome !== undefined) {
-  window.trektor.storage = new ChromeStorage();
-  window.trektor.runtime = new ChromeRuntime();
+  trektor.storage = new ChromeStorage(chrome.storage.local);
+  trektor.runtime = new ChromeRuntime(chrome.runtime);
 } else {
-  window.trektor.storage = browser.storage.local;
-  window.trektor.runtime = browser.runtime;
+  trektor.storage = browser.storage.local;
+  trektor.runtime = browser.runtime;
 }
+
+trektor.trelloGateway = new TrelloGateway(trektor.storage);
+trektor.togglGateway = new TogglGateway(trektor.storage);
+
+window.trektor = trektor;
